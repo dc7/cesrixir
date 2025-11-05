87ef364742d5d15754e32db6a9ad3b1c70e90abf
Charles Lanahan <charles.lanahan@gmail.com>
2025-11-05 08:54:12 -0500

    Initial Commit

---
+++ lib/cesr.ex
@@ -0,0 +1,18 @@
+defmodule Cesr do
+  @moduledoc """
+  Documentation for `Cesr`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cesr.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/cesr_test.exs
@@ -0,0 +1,8 @@
+defmodule CesrTest do
+  use ExUnit.Case
+  doctest Cesr
+
+  test "greets the world" do
+    assert Cesr.hello() == :world
+  end
+end
